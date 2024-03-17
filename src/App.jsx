a2382a38c9b97a43cf18350abed8bb3068234195
dsjramesh7 <rameshdussa07@gmail.com>
2024-03-17 14:04:37 +0530

    practice

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,38 @@ const App = () => {
   let nameOfChannel = "React Multiverse";
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>
-        Youtube Channel <span style={{ color: "black" }}>{nameOfChannel}</span>
-      </h1>
-      <div className="containerV">
-        <Video
-          textProp="kaame yaame haaa!!!!"
-          imgProp={"https://picsum.photos/id/237/200/120"}
-        />
+      <div className="app">
+        <h1 style={{ textAlign: "center" }}>
+          Youtube Channel{" "}
+          <span style={{ color: "black" }}>{nameOfChannel}</span>
+        </h1>
+        <div className="containerV">
+          <Video
+            title="Cute Dog"
+            image={"https://picsum.photos/id/237/200/120"}
+            channel="react mulitverse"
+            views={69}
+            time="1 year ago"
+            verified={true}
+          />
+
+          <Video
+            title="Cute Dog"
+            image={"https://picsum.photos/id/237/200/120"}
+            channel="react mulitverse"
+            views={69}
+            time="1 year ago"
+            verified={true}
+          />
+
+          <Video
+            title="Cute Dog"
+            image={"https://picsum.photos/id/237/200/120"}
+            channel="react mulitverse"
+            views={69}
+            time="1 year ago"
+          />
+        </div>
       </div>
     </>
   );
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,10 +1,17 @@
-// import DB  from "../assets/DBZ.jpg";
 import "./Video.css";
-const Video = ({ imgProp, textProp }) => {
+const Video = ({ image, title, channel, views, time, verified }) => {
   return (
     <div className="videoWrapper">
-      <img src={imgProp} alt="" />
-      <p className="videoname">{textProp}</p>
+      <img src={image} alt="" />
+      <div className="title">{title}</div>
+      {verified ? (
+        <div className="channel">{channel} ✅</div>
+      ) : (
+        <div className="channel">{channel}</div>
+      )}
+      <div className="views">
+        {views}k views <span>.</span> {time}
+      </div>
     </div>
   );
 };
